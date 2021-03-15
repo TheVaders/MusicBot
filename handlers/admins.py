52079875a0fdd6f0e6a3147aa13dd8fe2a336bb9
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-15 13:28:56 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -18,6 +18,15 @@ async def pause(client: Client, message: Message):
     tgcalls.pytgcalls.pause_stream(message.chat.id)
     await message.reply_text("**Hêllẞø†:** ⏸ Paused.")
 
+@Client.on_message(
+    filters.command("pause")
+    & filters.group
+    & ~ filters.edited
+)
+@errors
+async def pause_(client: Client, message: Message):
+    await message.reply_text("**Hêllẞø†:** Only Group Admins & Sudo Users can do that.")
+
 
 @Client.on_message(
     filters.command("resume")
@@ -29,6 +38,15 @@ async def pause(client: Client, message: Message):
 async def resume(client: Client, message: Message):
     tgcalls.pytgcalls.resume_stream(message.chat.id)
     await message.reply_text("**Hêllẞø†:** ▶️ Resumed.")
+
+@Client.on_message(
+    filters.command("resume")
+    & filters.group
+    & ~ filters.edited
+)
+@errors
+async def resume_(client: Client, message: Message):
+    await message.reply_text("**Hêllẞø†:** Only Group Admins & Sudo Users can do that.")
 
 
 @Client.on_message(
@@ -46,6 +64,15 @@ async def stop(client: Client, message: Message):
 
     tgcalls.pytgcalls.leave_group_call(message.chat.id)
     await message.reply_text("**Hêllẞø†:** ⏹ Stopped.")
+
+@Client.on_message(
+    filters.command(["stop", "end"])
+    & filters.group
+    & ~ filters.edited
+)
+@errors
+async def stop_(client: Client, message: Message):
+    await message.reply_text("**Hêllẞø†:** Only Group Admins & Sudo Users can do that.")
 
 
 @Client.on_message(
@@ -68,6 +95,15 @@ async def skip(client: Client, message: Message):
         )
 
     await message.reply_text("**Hêllẞø†:** ⏩ Skipped the current song.")
+
+@Client.on_message(
+    filters.command(["skip", "next"])
+    & filters.group
+    & ~ filters.edited
+)
+@errors
+async def pause_(client: Client, message: Message):
+    await message.reply_text("**Hêllẞø†:** Only Group Admins & Sudo Users can do that.")
 
 
 @Client.on_message(
